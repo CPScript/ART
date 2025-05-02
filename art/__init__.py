09c6a0b106418e76d512921040788c11283cd582
0x44 <83523587+CPScript@users.noreply.github.com>
2025-05-02 01:48:03 -0400

    Create __init__.py

---
+++ art/__init__.py
@@ -0,0 +1,7 @@
+from .color import ANSIColor
+from .symbols import Symbols, EMPTY_SET, EMPTY_CHAR
+from .config import Config, COLORS, REVERSE
+from .text_image import TextImage, Fragment, IndexedFragment
+from .converter import convert_image_to_ascii, PixelImage, FragmentWriter
+
+__version__ = "0.1.0"
